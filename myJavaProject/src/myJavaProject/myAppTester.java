ea4e2f469c98921f1fb86869247e894a0ebb2e22
Lakhvir Kaur <lakhvirkaur730@gmail.com>
2020-05-22 20:31:50 -0700

    program is written

--- myJavaProject/src/myJavaProject/myAppTester.java
+++ myJavaProject/src/myJavaProject/myAppTester.java
@@ -3,6 +3,8 @@
  */
 package myJavaProject;
 
+import java.util.Scanner;
+
 /**
  * @author lakhv
  *
@@ -14,7 +16,20 @@ public class myAppTester {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input =new Scanner(System.in);
+		int x=0;
+		System.out.println("Enter any number then i will tell you that number is odd or even");
+		x= input.nextInt();
+		oddEven(x);
 	}
-
+	
+	private static void oddEven(int a) {
+		// TODO Auto-generated method stub
+		if(a%2 ==0) {
+			System.out.println("number is even");
+		}
+		else {
+			System.out.println("number is odd");
+		}
+	}
 }
